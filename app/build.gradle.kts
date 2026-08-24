1a3ba670e9c740feeaaf84c17e23504acb432b9e
kencoppersmith25 <kencoppersmith@yahoo.com>
2026-08-24 09:45:47 -0500

    Bump version code to 5 for Play Store release (1.2.2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.coppersmith.music1chat"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
